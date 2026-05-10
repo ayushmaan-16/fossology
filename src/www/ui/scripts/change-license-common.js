c4e6a7935f1067ab4d9c266115ee77d9e8182e2d
Akash Kumar Sah <akashsah2003@gmail.com>
2026-05-10 20:22:31 +0530

    Latest fixes

--- src/www/ui/scripts/change-license-common.js
+++ src/www/ui/scripts/change-license-common.js
@@ -887,6 +887,7 @@ async function saveExpression() {
         .done(function (data) {
           $('#editExpressionModal').modal('hide');
           $('#newExpressionbtn').hide();
+          $('#newExpressionInfo').hide();
           $('#decTypeSet').addClass('border-danger');
           var table = createClearingTable();
           table.fnDraw(false);
